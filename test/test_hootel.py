f2387d74684d3b1e879762de58cb7c2bb0829eb6
horvathbregina <reginahorvathbako@gmail.com>
2025-04-14 18:24:23 +0200

    headless mode, max window

--- test/test_hootel.py
+++ test/test_hootel.py
@@ -2,6 +2,8 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import allure
 import pytest
 
@@ -13,6 +15,7 @@ class TestHootel(object):
         options.add_experimental_option("detach", True)
         options.add_argument("--headless")
         self.browser = webdriver.Chrome(options=options)
+        self.browser.maximize_window()
         self.browser.get(URL)
 
     def teardown_method(self):
@@ -23,7 +26,7 @@ class TestHootel(object):
     @allure.severity(allure.severity_level.TRIVIAL)
     @allure.tag("login")
     def test_login(self):
-        login_btn = self.browser.find_element(By.XPATH, '//a[@class="nav-link"]')
+        login_btn = WebDriverWait(self.browser,5).until(EC.element_to_be_clickable((By.XPATH,'//a[@class="nav-link"]')))
         login_btn.click()
 
         email_input = self.browser.find_element(By.ID, 'email')
